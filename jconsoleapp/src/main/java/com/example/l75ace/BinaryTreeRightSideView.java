9915b3c44d1ed053619ff09336c2c5c5acb55c1e
Legend <>
2024-03-07 20:49:45 +0300

    Organized Tree Methods in BinaryTreeRightSideView class

--- jconsoleapp/src/main/java/com/example/l75ace/BinaryTreeRightSideView.java
+++ jconsoleapp/src/main/java/com/example/l75ace/BinaryTreeRightSideView.java
@@ -12,18 +12,16 @@ import java.util.Queue;
 public class BinaryTreeRightSideView {
 
     public static void preRightSideView() {
-        TreeNode root = populateTree();
-        printNode(root);
-        p();
-        printNode2(root);
-        p();
-        printNode3(root);
-        p();
-        printNode4(root);
-        p();
-        printNode5(root);
-        p();
-        p(printTree(root));
+        TreeNode root = prePopulateTree();
+//        printTreeNodesDfsWNulls(root);
+//        p();
+//        printTreeNodesBfsNoNulls(root);
+//        p();
+//        printTreeNodesBfsWNulls(root);
+//        p();
+//        p(getTreeNodesToListNullsTrimmed(root));
+
+        printTreeNodesBfsNullsTrimmed(root);
         p();
 
 //        p(rightSideView(root));
@@ -55,155 +53,35 @@ public class BinaryTreeRightSideView {
     }
 
 
-    private static TreeNode populateTree() {
-////        [1, 2, 3, null, 5, null, 4]
-//
-//        TreeNode d = new TreeNode(4);
-//        TreeNode c = new TreeNode(5);
-//
-//        TreeNode b = new TreeNode(3, null, d);
-//        TreeNode a = new TreeNode(2, null, c);
-//
-//        TreeNode root = new TreeNode(1, a, b);
-//        return root;
-//        Integer[] nodes = {1, 2, 3, -1, 5, -1, 4};
+    private static TreeNode prePopulateTree() {
 //        Integer[] nodes = {1, 2, 3, null, 5, null, 4};
-//        return insertNodes(nodes);
-//        return insertFromArray(nodes);
+//        return populateTreeBfs(nodes);
 
-
-////        [1, null, 3]
-//        TreeNode a = new TreeNode(3);
-//
-//        TreeNode root = new TreeNode(1, null, a);
-//        return root;
 //        Integer[] nodes = {1, null, 3};
-//        return insertNodes(nodes);
-//        Integer[] nodes = {1, null, 3};
-//        return insertFromArray(nodes);
+//        return populateTreeBfs(nodes);
 
-////        []
-//        TreeNode root = null;
-//        return root;
 //        Integer[] nodes = {};
-//        return insertNodes(nodes); //not working
-//        Integer[] nodes = {};
-//        return insertFromArray(nodes);
+//        return populateTreeBfs(nodes);
 
-////        [1, 2, null]
-//        TreeNode a = new TreeNode(2);
-//
-//        TreeNode root = new TreeNode(1, a, null);
-//        return root;
-//        Integer[] nodes = {1, 2, null};
-//        return insertNodes(nodes);
-//        Integer[] nodes = {1, 2, null};
-//        return insertFromArray(nodes);
 
-////        [1, 2, 3, null, null, 4, null]
-//        TreeNode c = new TreeNode(4);
-//
-//        TreeNode b = new TreeNode(3, c, null);
-//        TreeNode a = new TreeNode(2);
-//
-//        TreeNode root = new TreeNode(1, a, b);
-//        return root;
-//        Integer[] nodes = {1, 2, 3, null, null, 4, null}; //
-//        return insertNodes(nodes);
+//        Integer[] nodes = {1, 2, null};
+//        return populateTreeBfs(nodes);
+
 //        Integer[] nodes = {1, 2, 3, null, null, 4, null};
-//        return insertFromArray(nodes);
+//        return populateTreeBfs(nodes);
 
-//        [1, 2, 3, null, 4, null, null]
 //        Integer[] nodes = {1, 2, 3, null, 4, null, null};
-//        return insertNodes(nodes);
-//        Integer[] nodes = {1, 2, 3, null, 4, null, null};
-//        return insertFromArray(nodes);
+//        return populateTreeBfs(nodes);
 
-//        [1, 2, 3, 4, null, null, null]
 //        Integer[] nodes = {1, 2, 3, 4, null, null, null};
-//        return insertNodes(nodes);
-//        Integer[] nodes = {1, 2, 3, 4, null, null, null};
-//        return insertFromArray(nodes);
+//        return populateTreeBfs(nodes);
 
-//        [1, 2, 3, 4, 5, null, null, null, null, null, 6, 7, 8, 9, null, 10]
-//
-//        TreeNode i = new TreeNode(10);
-//
-//        TreeNode h = new TreeNode(9);
-//
-//        TreeNode g = new TreeNode(8, i, null);
-//        TreeNode f = new TreeNode(7, h, null);
-//
-//        TreeNode e = new TreeNode(6, f, g);
-//
-//        TreeNode d = new TreeNode(5, null, e);
-//        TreeNode c = new TreeNode(4);
-//
-//        TreeNode b = new TreeNode(3);
-//        TreeNode a = new TreeNode(2, c, d);
-//
-//        TreeNode root = new TreeNode(1, a, b);
-//        return root;
         Integer[] nodes = {1, 2, 3, 4, 5, null, null, null, null, null, 6, 7, 8, 9, null, 10};
-//        return insertNodes(nodes);
-        return insertFromArray(nodes);
+        return populateTreeBfs(nodes);
     }
 
-    // wrong
-//    private static TreeNode insertNode(Integer[] nodes) {
-//        Queue<TreeNode> nodesQueue = new LinkedList<>();
-//        int i = 0;
-////        Integer[] nodes = {1, 2, 3, null, 5, null, 4};
-//
-//        TreeNode treeRoot = new TreeNode(nodes[i]);
-//        nodesQueue.offer(treeRoot);
-//
-//        while (!nodesQueue.isEmpty()) {
-//            TreeNode currentNode = nodesQueue.poll();
-//            TreeNode root = null;
-////            p(currentNode);
-//            if (currentNode != null) {
-//                root = currentNode;
-//                i++;
-//                if (i < nodes.length) {
-//                    TreeNode left = null;
-//                    if (nodes[i] != null) {
-//                        left = new TreeNode(nodes[i]);
-//                    }
-//                    root.left = left;
-//                    nodesQueue.offer(left);
-//                    i++;
-//                    if (i < nodes.length) {
-//                        TreeNode right = null;
-//                        if (nodes[i] != null) {
-//                            right = new TreeNode(nodes[i]);
-//                        }
-//                        root.right = right;
-//                        nodesQueue.offer(right);
-////                        i++;
-//                    }
-//                }
-//            } else {
-//                i++;
-//                if (i < nodes.length) {
-//                    if (nodes[i] != null)
-//                        nodesQueue.offer(new TreeNode(nodes[i]));
-//                    else
-//                        nodesQueue.offer(null);
-//                }
-//            }
-//
-////            if (i < 4)
-////                treeRoot = root;
-//        }
-//
-//        return treeRoot;
-//    }
-
-
-
-//    public static TreeNode insertFromArray(int[] arr) {
-    public static TreeNode insertFromArray(Integer[] arr) {
+    // *B Iterative BFS. insert nodes from array to tree.
+    public static TreeNode populateTreeBfs(Integer[] arr) {
         if (arr == null || arr.length == 0) {
             return null;
         }
@@ -212,14 +90,12 @@ public class BinaryTreeRightSideView {
         queue.offer(root);
         int i = 1;
         while (i < arr.length) {
-            TreeNode curr = queue.poll();
-            if (curr != null) {
-//                TreeNode leftChild = (i < arr.length && arr[i] != -1) ? new TreeNode(arr[i]) : null;
-//                TreeNode rightChild = (i + 1 < arr.length && arr[i + 1] != -1) ? new TreeNode(arr[i + 1]) : null;
+            TreeNode currentRoot = queue.poll();
+            if (currentRoot != null) {
                 TreeNode leftChild = (i < arr.length && arr[i] != null) ? new TreeNode(arr[i]) : null;
                 TreeNode rightChild = (i + 1 < arr.length && arr[i + 1] != null) ? new TreeNode(arr[i + 1]) : null;
-                curr.left = leftChild;
-                curr.right = rightChild;
+                currentRoot.left = leftChild;
+                currentRoot.right = rightChild;
                 queue.offer(leftChild);
                 queue.offer(rightChild);
                 i += 2;
@@ -228,70 +104,19 @@ public class BinaryTreeRightSideView {
         return root;
     }
 
-    // wrong
-    private static TreeNode insertNode(Integer[] nodes) {
-        Queue<TreeNode> nodesQueue = new LinkedList<>();
-        int i = 0;
-//        Integer[] nodes = {1, 2, 3, null, 5, null, 4};
-//        [1, 2, 3, 4, 5, null, null, null, null, null, 6, 7, 8, 9, null, 10]
 
-        TreeNode treeRoot = new TreeNode(nodes[i]);
-        nodesQueue.offer(treeRoot);
-
-        while (!nodesQueue.isEmpty()) {
-            TreeNode currentRoot = nodesQueue.poll();
-//            p(currentRoot);
-            if (currentRoot != null) {
-                i++;
-                if (i < nodes.length) {
-                    TreeNode left = null;
-                    if (nodes[i] != null) {
-                        left = new TreeNode(nodes[i]);
-                    }
-                    currentRoot.left = left;
-                    nodesQueue.offer(left);
-                    i++;
-                    if (i < nodes.length) {
-                        TreeNode right = null;
-                        if (nodes[i] != null) {
-                            right = new TreeNode(nodes[i]);
-                        }
-                        currentRoot.right = right;
-                        nodesQueue.offer(right);
-                    }
-                }
-            } else {
-                i++;
-                if (i < nodes.length) {
-                    if (nodes[i] != null)
-                        nodesQueue.offer(new TreeNode(nodes[i]));
-                    else // may not be needed
-                        nodesQueue.offer(null);
-                }
-            }
-
-        }
-
-        return treeRoot;
-    }
-
-    // wrong
-    private static TreeNode insertNodes(Integer[] nodes) {
-        return insertNode(nodes);
-    }
-
-    // DFS With null nodes
-    private static void printNode(TreeNode root) {
+    // Recursive DFS With null nodes
+    private static void printTreeNodesDfsWNulls(TreeNode root) {
         if (root != null) {
             System.out.print(root.val + " ");
-            printNode(root.left);
-            printNode(root.right);
+            printTreeNodesDfsWNulls(root.left);
+            printTreeNodesDfsWNulls(root.right);
         } else
             System.out.print("null ");
     }
 
-    // BFS print Without null nodes
-    private static void printNode2(TreeNode root) {
+    // Iterative BFS print Without null nodes
+    private static void printTreeNodesBfsNoNulls(TreeNode root) {
         if (root == null)
             return;
 
@@ -311,56 +136,10 @@ public class BinaryTreeRightSideView {
         }
     }
 
-    // wrong
-    private static void printNode3(TreeNode root) {
+    // Iterative BFS print With null nodes and Without Trimming null nodes at the end
+    private static void printTreeNodesBfsWNulls(TreeNode root) {
         if (root == null)
             return;
-
-        Queue<TreeNode> nodes = new LinkedList<>();
-        nodes.offer(root);
-
-        while (!nodes.isEmpty()) {
-            TreeNode currentNode = nodes.poll();
-            if (currentNode != null) {
-                pl(currentNode.val);
-                if (currentNode.left != null || currentNode.right != null) {
-                    nodes.offer(currentNode.left);
-                    nodes.offer(currentNode.right);
-                }
-            } else
-                pl(null);
-        }
-    }
-
-    // wrong
-    private static void printNode4(TreeNode root) {
-        if (root == null)
-            return;
-//        Integer[] nodes = {1, 2, 3, null, null, 4, null}; /////
-
-        Queue<TreeNode> nodesQueue = new LinkedList<>();
-        nodesQueue.offer(root);
-
-        while (!nodesQueue.isEmpty()) {
-            TreeNode currentNode = nodesQueue.poll();
-            if (currentNode != null) {
-                pl(currentNode.val);
-                if (currentNode.left != null || currentNode.right != null) {
-//                if (currentNode.left != null || currentNode.right != null || !nodesQueue.isEmpty()) {
-                    nodesQueue.offer(currentNode.left);
-                    nodesQueue.offer(currentNode.right);
-                }
-            } else
-                pl(null);
-        }
-    }
-
-    // BFS print With null nodes and Without Trimming null nodes at the end
-    private static void printNode5(TreeNode root) {
-        if (root == null)
-            return;
-//        Integer[] nodes = {1, 2, 3, null, null, 4, null}; /////
-//        [1, 2, 3, null, 5, null, 4]
 
         Queue<TreeNode> nodesQueue = new LinkedList<>();
         nodesQueue.offer(root);
@@ -383,8 +162,8 @@ public class BinaryTreeRightSideView {
         }
     }
 
-    // *B BFS print With null nodes and With Trimming trailing null nodes
-    public static List<Integer> printTree(TreeNode root) {
+    // Iterative BFS. Get Tree nodes as List With null nodes and With Trimming trailing null nodes
+    public static List<Integer> getTreeNodesToListNullsTrimmed(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result;
@@ -408,6 +187,34 @@ public class BinaryTreeRightSideView {
             lastIndex--;
         }
         return result;
+    }
+
+    // *B Iterative BFS print With null nodes and With Trimming trailing null nodes (like Leetcode)
+    public static void printTreeNodesBfsNullsTrimmed(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            p(null);
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            if (curr != null) {
+                result.add(curr.val);
+                queue.offer(curr.left);
+                queue.offer(curr.right);
+            } else {
+                result.add(null);
+            }
+        }
+        // Trim trailing nulls
+        int lastIndex = result.size() - 1;
+        while (lastIndex >= 0 && result.get(lastIndex) == null) {
+            result.remove(lastIndex);
+            lastIndex--;
+        }
+        p(result);
     }
 
 }
