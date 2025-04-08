79038de9795dac1826ae9309fbb72928e1fb019e
Moaaz Atik <moaaz.atik@gmail.com>
2025-04-08 18:50:02 +0300

    #BracketMatcher @ Coderbyte

---
+++ jconsoleapp/src/main/java/com/example/jconsoleapp/others/BracketMatcher.java
@@ -0,0 +1,40 @@
+package com.example.jconsoleapp.others;
+
+import java.util.Stack;
+
+// #BracketMatcher @ Coderbyte
+public class BracketMatcher {
+
+    public static String bracketMatcher(String str) {
+        int b = 0;
+        for (char c : str.toCharArray()) {
+            if (c == '(') // right-facing brackets
+                b++;
+            else if (c == ')')
+                b--;
+
+            if (b == -1) // or < 0
+                return "0";
+        }
+
+        return b == 0 ? "1" : "0";
+    }
+
+    public static String bracketMatcher2(String str) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+
+            if (c == '(')
+                stack.push(0);
+
+            else if (c == ')')
+                if (!stack.isEmpty())
+                    stack.pop();
+                else return "0";
+        }
+
+        return stack.isEmpty() ? "1" : "0";
+    }
+
+}
